ef2e6642e09b5f0a18b7cf285f1691dd6f6124bb
Kirill Tatchihin <kirabsuir@gmail.com>
2019-05-09 12:22:16 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# python-chillout
+Just notes from learning python while chilling
---
+++ lists.py
@@ -0,0 +1,5 @@
+numbers = ["first", "second", "third"]
+numbers.insert(1, 1) # extra insert
+
+for my_number in numbers:
+    print(my_number)
