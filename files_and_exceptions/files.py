cec1d1ab74fec85a1ba35abb44a1cab7ac2818c7
Kirill Tatchihin <kirabsuir@gmail.com>
2019-06-19 22:51:37 +0300

    Add comments to files

--- files_and_exceptions/files.py
+++ files_and_exceptions/files.py
@@ -1,14 +1,19 @@
+# Try to find file and open it
+
 try:
 	data = open('sketch.txt')
 	for each_line in data:
+		# Try to iterate over file data
 		try:
 			(role, line_spoken) = each_line.split(':', 1) 
 			print(role, end='')
 			print(' said: ', end='')
 			print(line_spoken, end='')
+		# Except situation when line cannot be splitted correctly
 		except ValueError:
 			pass
 	data.close()
+# Except situation, when file not exists	
 except IOError:
 	print('File not found!')
 	pass
